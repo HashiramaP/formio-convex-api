26e046785a8dd9e457d9f9e9961a8093e81515d4
Msa360 <arnaud25@icloud.com>
2026-05-07 16:19:00 -0400

    sync api.ts

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,12 +13,15 @@ import type * as clients from "../clients.js";
 import type * as errorLogs from "../errorLogs.js";
 import type * as feedback from "../feedback.js";
 import type * as firms from "../firms.js";
+import type * as formDefinitions from "../formDefinitions.js";
 import type * as legalDocuments from "../legalDocuments.js";
 import type * as migrations from "../migrations.js";
+import type * as questionTemplates from "../questionTemplates.js";
 import type * as questions from "../questions.js";
 import type * as submissionDocuments from "../submissionDocuments.js";
 import type * as submissions from "../submissions.js";
 import type * as supplementRequests from "../supplementRequests.js";
+import type * as uploadedForms from "../uploadedForms.js";
 
 import type {
   ApiFromModules,
@@ -32,12 +35,15 @@ declare const fullApi: ApiFromModules<{
   errorLogs: typeof errorLogs;
   feedback: typeof feedback;
   firms: typeof firms;
+  formDefinitions: typeof formDefinitions;
   legalDocuments: typeof legalDocuments;
   migrations: typeof migrations;
+  questionTemplates: typeof questionTemplates;
   questions: typeof questions;
   submissionDocuments: typeof submissionDocuments;
   submissions: typeof submissions;
   supplementRequests: typeof supplementRequests;
+  uploadedForms: typeof uploadedForms;
 }>;
 
 /**
